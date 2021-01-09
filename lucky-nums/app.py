c9e77339b53a075a802660bf9da114501b917e99
Jason Benjoya <jasonbenjoya@gmail.com>
2021-01-09 17:07:26 -0500

    added comments to code and did some slight refactoring to clean up WET code

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -16,14 +16,20 @@ def homepage():
 
 @app.route('/api/get-lucky-num', methods=['POST'])
 def get_num_facts():
+    """api route to gather lucky number facts based on form info
+
+return json with year and num fact
+"""
+    # declare array to keep track of any errors
     errors = {}
 
+    # gather data from json request body
     name = request.json.get('name')
     email = request.json.get('email')
     year = request.json.get('year')
     color = request.json.get('color')
 
-    print(name, email, year, color)
+    # handle errors based on required fields and specific color/year params
     if not name:
         errors['name'] = ['This is a required field']
     if not email:
@@ -37,25 +43,34 @@ def get_num_facts():
         errors['year'] = ['This is a required field']
     else:
         try:
+            # convert json year input to an integer
             year = int(year)
             if year < 1900 or year > 2000:
                 errors['year'] = ['Please enter a year between 1900 and 2000']
         except ValueError:
             errors['year'] = ['Please enter a numeric value']
 
+    # retun errors dict if any errors exist
     if errors:
         return jsonify(errors=errors)
 
+    # declare empty dict for json of facts to be returned
     lucky = {}
+    # get lucky number
     num = random.randint(1, 100)
 
+    # ping API to get facts about the birth year
     year_fact = requests.get(f'{DEFAULT_LUCKY_URL}/{year}/year')
     year_text = year_fact.text
 
+    # ping API to get facts about lucky num
     num_fact = requests.get(f'{DEFAULT_LUCKY_URL}/{num}')
     num_text = num_fact.text
 
+    # add year and num facts to dict
     lucky['year'] = {'fact': year_text, 'year': year}
     lucky['num'] = {'fact': num_text, 'num': num}
+
+    # return the dict in json form with 201 status code
 
     return (jsonify(lucky), 201)
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -11,7 +11,10 @@ const $results = $('#lucky-results');
 
 /** processForm: get data from form and make AJAX call to our API. */
 async function processForm(evt) {
+	// prevent default form submission
 	evt.preventDefault();
+
+	// define data object to submit along with request using form values
 	data = {
 		name  : $name.val(),
 		year  : $year.val(),
@@ -19,8 +22,10 @@ async function processForm(evt) {
 		color : $color.val()
 	};
 
+	// ping the back-end API with the form data
 	let resp = await axios.post('/api/get-lucky-num', data);
-	console.log(resp);
+
+	// run the handleResponse function to manipulate dom
 	handleResponse(resp);
 }
 
@@ -29,23 +34,31 @@ async function processForm(evt) {
 function handleResponse(resp) {
 	let text = resp.data;
 	$('b').html(''); //reset errors for each submit
+	// reset results for each submit
 	$results.text('');
+
+	// deal with errors returned from api and display on page
 	if (text.errors) {
+		// declare variable for clarity purposes
 		const errors = text.errors;
 		for (let err_list in errors) {
 			for (let err of errors[err_list]) {
 				$(`#${err_list}-err`).append(err);
 			}
 		}
+		// add the form data to the dom if no errors exist
 	} else {
+		// separate num fact API response from year fact API response
 		let numData = text.num;
 		let yearData = text.year;
 
 		let numFact = `Your lucky number is ${numData.num} (${numData.fact})`;
 		let yearFact = `Your birth year, ${yearData.year}, fact is: ${yearData.fact}`;
 
+		// append facts to DOM in plain text form (with a break in between to make it look neater)
 		$results.append(numFact).append('<br>').append(yearFact);
 	}
 }
 
+// handle the form submission
 $('#lucky-form').on('submit', processForm);
